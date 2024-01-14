73fce30b20c677bf590f220c2d5ea9741fce92af
Vishnu Murali <vishnumurali880@gmail.com>
2024-01-14 21:08:58 +0530

    updating workflow

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask,request,render_template
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
+# app=application
 
 ## Route for a home page
 
